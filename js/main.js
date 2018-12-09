443a10677f3cdf030ff7e7d07f08919b18567345
Olehkozyk <oleg.kozyk@gmail.com>
2018-12-09 11:59:39 +0200

    'slide'

---
+++ js/main.js
@@ -0,0 +1,14 @@
+;(function($){
+"use strict";
+$(function(){
+    $('.ba-team-slider').slick({
+        infinity: false,
+        slidesToScroll: 1,
+        slidesToShow: 1,
+        dots: true,
+        
+        
+    })
+});
+})(jQuery);
+
